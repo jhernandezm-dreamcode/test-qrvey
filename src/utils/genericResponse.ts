846d1eb14bc8b87059e7128c6c3468d47e8f03f1
Jorge Hernandez <jorge.hernandez@dreamcodesoft.com>
2021-06-11 16:53:48 -0500

    generic response

---
+++ src/utils/genericResponse.ts
@@ -0,0 +1,31 @@
+import { Headers } from "../config/settings";
+
+/**
+ * @name GenericFunctions
+ * @description class for generic functions
+ */
+class GenericFunctions {
+  /**
+   * @name setResponse
+   * @description method for create a response
+   * @param code
+   * @param status
+   * @param body
+   * @returns {Object}
+   */
+  public async setResponse(code: number, status: string, body: any): Promise<any> {
+    let response: any = {
+      Headers,
+    };
+    let bodyResponse: any = JSON.stringify({
+      code: code,
+      status: status,
+      body: body,
+    });
+    response.body = bodyResponse;
+    return response;
+  }
+}
+
+const genericFunctions = new GenericFunctions();
+export default genericFunctions;
